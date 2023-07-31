5d010bc6484585c3a6b5c0e1f77a353b5c815b1b
clawrence00 <>
2023-07-31 18:59:09 +0000

    initial commit

---
+++ mototorque/__init__.py
@@ -0,0 +1,23 @@
+from flask_login import LoginManager
+import os
+from flask import flask
+from flask_sqlalchemy import SQLAlchemy
+if os.path.exists("env.py"):
+    import env
+
+app = Flask(__name__)
+app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
+
+if os.environ.get("DEVELOPMENT") == "True":
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")  # local
+else:
+    uri = os.environ.get("DATABASE_URL")
+    if uri.startswith("postgres://"):
+        uri = uri.replace("postgres://", "postgresql://", 1)
+    app.config["SQLALCHEMY_DATABASE_URI"] = uri  # heroku
+
+db = SQLAlchemy(app)
+
+login_manager = LoginManager()
+
+from taskmanager import routes  # noqa
