ff1dc9b261a12880b8e1b8148f5b0bbb6675fafc
clawrence00 <>
2023-08-01 18:54:33 +0000

    added modal for browse button, styled nav links as buttons

--- mototorque/static/js/script.js
+++ mototorque/static/js/script.js
@@ -1,4 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
     var sidenav = document.querySelectorAll('.sidenav');
     M.Sidenav.init(sidenav);
+    var modal = document.querySelectorAll('.modal');
+    M.Modal.init(modal);
 });
