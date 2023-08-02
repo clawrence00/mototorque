8a42a1c82a37a37a6c228ce3eb6df739a0dc22ca
clawrence00 <>
2023-08-02 21:03:34 +0000

    trying to render data entry

--- mototorque/routes.py
+++ mototorque/routes.py
@@ -27,9 +27,14 @@ def add_word():
     return render_template("add.html")
 
 
-@ app.route('/browse', methods=['GET', 'POST'])
+# @ app.route('/browse', methods=['GET', 'POST'])
+# def browse():
+#     selected_letter = request.args.get('type')
+#     print(selected_letter)  # <-- should print letter
+#     word = list(Dictionary.query.order_by(Dictionary.word_phrase).all())
+#     return render_template("browse.html", letter=selected_letter, word=word)
+
+@ app.route('/browse')
 def browse():
-    selected_letter = request.args.get('type')
-    print(selected_letter)  # <-- should print letter
-    word = list(Dictionary.query.order_by(Dictionary.word_phrase).all())
-    return render_template("browse.html", letter=selected_letter)
+    entry = list(Dictionary.query.order_by(Dictionary.word_phrase).all())
+    return render_template("browse.html", entry=entry)
