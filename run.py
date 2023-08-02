32071885024c78af640ea094343f54dcbcc1a77c
clawrence00 <>
2023-08-02 19:22:47 +0000

    adding potential solution to error

--- run.py
+++ run.py
@@ -2,6 +2,10 @@ import os
 from mototorque import app
 
 if __name__ == "__main__":
+    def create_tables(self):
+    with app.app_context():
+        db.create_all()
+    create_tables(db)
     app.run(
         host=os.environ.get("IP"),
         port=int(os.environ.get("PORT")),
