259605a851ef310dea441fa5db57ad14209f6873
clawrence00 <>
2023-08-02 19:24:24 +0000

    corrected indentation

--- run.py
+++ run.py
@@ -3,11 +3,11 @@ from mototorque import app
 
 if __name__ == "__main__":
     def create_tables(self):
-    with app.app_context():
-        db.create_all()
-    create_tables(db)
-    app.run(
-        host=os.environ.get("IP"),
-        port=int(os.environ.get("PORT")),
-        debug=os.environ.get("DEBUG")
-    )
+        with app.app_context():
+            db.create_all()
+        create_tables(db)
+        app.run(
+            host=os.environ.get("IP"),
+            port=int(os.environ.get("PORT")),
+            debug=os.environ.get("DEBUG")
+        )
